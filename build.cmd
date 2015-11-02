2ee38152659da839c91890572855d71df290a55d
halcwb <halcwb@gmail.com>
2015-11-02 09:11:38 +0100

    Trying to get build commands working

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 "C:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe" --define:BOOTSTRAP bootstrap.fsx
-./packages/FAKE/tools/Fake.exe build.fsx
+packages/FAKE/tools/FAKE.exe build.fsx %s
